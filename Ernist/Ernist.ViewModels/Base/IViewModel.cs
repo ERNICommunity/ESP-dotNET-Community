a35a9a238380670f067574e2f44d60d2d1937a64
Diego Bao <diego.bao@gmail.com>
2017-08-28 23:26:09 +0200

    Nuke Ernist

--- Ernist/Ernist.ViewModels/Base/IViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace Ernist.ViewModels.Base
-{
-    public interface IViewModel
-    {
-        event PropertyChangedEventHandler PropertyChanged;
-    }
-}
--- Ernist/Ernist.ViewModels/IProjectViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Ernist.ViewModels
-{
-    internal interface IProjectViewModel
-    {
-    }
-}
