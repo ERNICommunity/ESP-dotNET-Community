80da771fdda24be5b321bc47e56f34b1ca5aa04f
NyoronDev <nyoronsheppard@outlook.es>
2017-08-29 17:55:09 +0200

    Update UWP view.

---
+++ WarColors/WarColors/WarColors.UWP/CustomListViewRenderer.cs
@@ -0,0 +1,21 @@
+﻿
+using WarColors.UWP;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(DLToolkit.Forms.Controls.FlowListView), typeof(CustomListViewRenderer))]
+namespace WarColors.UWP
+{
+    public class CustomListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (List != null)
+            {
+                List.SelectionMode = Windows.UI.Xaml.Controls.ListViewSelectionMode.None;
+            }
+        }
+    }
+}
