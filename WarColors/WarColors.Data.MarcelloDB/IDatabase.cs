d6fa877c1104b11e322e99115f5eeba775e4c070
Diego Bao Montero <diego.bao@erni-espana.es>
2017-08-30 10:59:20 +0200

    Implement Dispose in Database

--- WarColors/WarColors.Data.MarcelloDB/IDatabase.cs
+++ WarColors/WarColors.Data.MarcelloDB/IDatabase.cs
@@ -1,10 +1,11 @@
 ﻿using MarcelloDB.Collections;
 using MarcelloDB.Index;
+using System;
 using WarColors.Data.Repositories;
 
 namespace WarColors.Data.Marcello
 {
-    public interface IDatabase
+    public interface IDatabase : IDisposable
     {
         Collection<TEntity, TKey, TIndexDef> GetCollection<TEntity, TKey, TIndexDef>()
             where TEntity : IEntity<TKey>
